1c7270d66a7465ac9b024addea1971d6a93c544b
Ronaldo Chan <ronaldopchan@outlook.com>
2024-09-20 14:35:37 +0800

    make restore executable

--- aws-rds/user_data.sh
+++ aws-rds/user_data.sh
@@ -3,3 +3,5 @@
 sudo yum install -y httpd php8.3 php8.3-cli php8.3-modphp.x86_64 php8.3-mysqlnd.x86_64 git-core.x86_64 htop mariadb105-server-utils.x86_64
 sudo chkconfig httpd on
 sudo systemctl start httpd
+git clone https://github.com/rpchan44/intro-to-cloud-on-aws.git
+sudo chmod +x intro-to-cloud-on-aws/aws-rds/restore
