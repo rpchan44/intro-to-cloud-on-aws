6e4b074170445af06838c5942c0dbc260d652d9d
Ronaldo Chan <ronaldopchan@outlook.com>
2024-09-20 12:35:56 +0800

    Add mysql client

--- aws-rds/user_data.sh
+++ aws-rds/user_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo yum install -y httpd php8.3 php8.3-cli php8.3-modphp.x86_64 php8.3-mysqlnd.x86_64 git-core.x86_64 htop
+sudo yum install -y httpd php8.3 php8.3-cli php8.3-modphp.x86_64 php8.3-mysqlnd.x86_64 git-core.x86_64 htop mariadb105-server-utils.x86_64
 sudo chkconfig httpd on
 sudo systemctl start httpd
