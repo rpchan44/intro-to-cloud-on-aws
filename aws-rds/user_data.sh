e1ce990782c3bb3e3eef8aa0c381010ac989fdf6
Ronaldo Chan <ronaldopchan@outlook.com>
2024-09-20 12:11:51 +0800

    update spelling errors

--- aws-rds/user_data.sh
+++ aws-rds/user_data.sh
@@ -2,4 +2,4 @@
 
 sudo yum install -y httpd php8.3 php8.3-cli php8.3-modphp.x86_64 php8.3-mysqlnd.x86_64 git-core.x86_64 htop
 sudo chkconfig httpd on
-sudo systemctl httpd start
+sudo systemctl start httpd
